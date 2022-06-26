8ca6b3c10b0ac1bc4b93646fb5302660a6580bc1
SDKSSH <sdk_ssh@protonmail.com>
2022-06-26 21:24:57 +0200

    Working to  bot

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,1 +1,16 @@
 import { SapphireClient } from '@sapphire/framework';
+import { Config } from '../config/Config';
+
+export class Bot {
+    private config: Config;
+    public client : SapphireClient;
+
+    constructor(config: Config){
+        this.config = config;
+        this.client = new SapphireClient({ intents: ['DIRECT_MESSAGES', 'GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
+    }
+
+    async init(){
+        await this.client.login(this.config.DISCORD_TOKEN);
+    }
+}
