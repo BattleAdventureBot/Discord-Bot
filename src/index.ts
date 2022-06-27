4ed4de6715d13a05c5963c743c59276c0e0699f9
SDKSSH <sdk_ssh@protonmail.com>
2022-06-27 09:58:50 +0200

    Init database on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Bot } from "./bot/Bot";
 import { loadConfig } from "./config/Config";
+import { Database } from "./database/Database";
 
 const config = loadConfig();
 
 const bot = new Bot(config);
 bot.init()
+
+new Database(config);
