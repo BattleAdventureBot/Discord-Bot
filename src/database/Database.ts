611d87ca4dffefb4a253f32973660134861711f1
SDKSSH <sdk_ssh@protonmail.com>
2022-06-27 10:01:22 +0200

    Edit name of field sequelize

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 import { Config } from "../config/Config";
 
 export class Database {
-    private sequelize : Sequelize;
+    public DatabaseClient : Sequelize;
     public static instance : Database;
 
     constructor(config: Config){
-        this.sequelize = new Sequelize(config.DATABASE_DATABASE, config.DATABASE_USER, config.DATABASE_PASSWORD, {
+        this.DatabaseClient = new Sequelize(config.DATABASE_DATABASE, config.DATABASE_USER, config.DATABASE_PASSWORD, {
             host: config.DATABASE_HOST,
             dialect: "mariadb",
         });
