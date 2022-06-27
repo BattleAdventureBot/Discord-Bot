1eeade82bf98863221eb95a2f426e5b258f17f66
SDKSSH <sdk_ssh@protonmail.com>
2022-06-27 10:08:25 +0200

    Add Model User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,40 @@
-import { Model } from "sequelize";
+import { Model, Sequelize, DataTypes } from "sequelize";
 
-export class User extends Model {
+export class UserInfo extends Model {
     public readonly id!: number;
     public readonly user_id!: number;
     public cash!: number;
     public level! : number;
     public exp! : number;
 }
+
+export function init(sequelize : Sequelize){
+    UserInfo.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        cash: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        level: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1
+        },
+        exp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        }
+    }, { sequelize, modelName: "user_info" });
+}
+
+export default UserInfo;
