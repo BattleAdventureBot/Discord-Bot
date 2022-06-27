3ce24dfd164ef51998ab3674c23764d0e0d6ccfd
SDKSSH <sdk_ssh@protonmail.com>
2022-06-27 09:58:23 +0200

    Init database

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,1 +1,15 @@
 import { Sequelize } from "sequelize";
+import { Config } from "../config/Config";
+
+export class Database {
+    private sequelize : Sequelize;
+    public static instance : Database;
+
+    constructor(config: Config){
+        this.sequelize = new Sequelize(config.DATABASE_DATABASE, config.DATABASE_USER, config.DATABASE_PASSWORD, {
+            host: config.DATABASE_HOST,
+            dialect: "mariadb",
+        });
+        Database.instance = this;
+    }
+}
